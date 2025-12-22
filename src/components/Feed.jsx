44ab0e1c748b204d3d1bb1fc40d75e0859b09418
rujesh <rujesh@afreespace.com>
2025-12-22 20:20:38 +0530

    send request from feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -26,6 +26,10 @@ const Feed = () => {
   getFeed();
   }, [])
 
+      if(!feed) return;
+    
+    if(feed.length <=0) return <h1 className='justify-center my-10'>No more users found!</h1>
+
   return (
    feed && ( <div>
       <UserCard feed={feed[0]}/>
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,7 +1,27 @@
+import axios from 'axios';
 import React from 'react'
+import { BASE_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { removeUserFromFeed } from '../utils/feedSlice';
 
-const UserCard = ({feed}) => {
-    const {firstName, lastName, photoUrl, age, gender, about} = feed;
+const UserCard = ({ feed }) => {
+    const {_id, firstName, lastName, photoUrl, age, gender, about } = feed;
+    const dispatch = useDispatch();
+    
+    
+    const handleSendRequest = async (status, userId) => {
+        try {
+            const res = await axios.post(BASE_URL + "/request/send/" + status + "/" + userId, {}, { withCredentials: true })
+            dispatch(removeUserFromFeed(userId))
+
+        } catch (e) {
+            console.error(e.message);
+
+        }
+    }
+
+
+
     return (
         <div className='flex justify-center my-10'>
             <div className="card bg-base-300 w-80 shadow-sm">
@@ -12,11 +32,11 @@ const UserCard = ({feed}) => {
                 </figure>
                 <div className="card-body">
                     <h2 className="card-title">{firstName + " " + lastName}</h2>
-                    <p>{age + ", "+ gender}</p>
+                    <p>{age + ", " + gender}</p>
                     <p>{about}</p>
                     <div className="card-actions justify-center">
-                        <button className="btn btn-primary">Ignore</button>
-                        <button className="btn btn-secondary">Interested</button>
+                        <button className="btn btn-primary" onClick={()=> handleSendRequest("ignored", _id)}>Ignore</button>
+                        <button className="btn btn-secondary" onClick={()=> handleSendRequest("interested", _id)} >Interested</button>
 
                     </div>
                 </div>
